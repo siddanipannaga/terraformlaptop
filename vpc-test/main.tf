bab5e6662467191a65147f8f0f5f651040568642
chandrasekhar2403 <pannaga.chandr24@gmail.com>
2024-01-23 22:13:40 +0530

    ter lsp

---
+++ vpc-test/main.tf
@@ -0,0 +1,19 @@
+module "roboshop" {
+    source = "../terraform-aws-vpc"
+    project_name = var.project_name
+    environment = var.Environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+
+    #public subnet
+    public_subnets_cidr = var.public_subnets_cidr
+
+    private_subnets_cidr = var.private_subnets_cidr
+
+    database_subnets_cidr = var.database_subnets_cidr
+    
+    
+    #peering
+    is_peering_required = var.is_peering_required
+  
+}
